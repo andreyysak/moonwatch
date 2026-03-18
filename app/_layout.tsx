d4021de853bc65dd17c3c01380d439876825bdc7
Andriy Sak <andreyysak17@gmail.com>
2026-03-18 17:17:27 +0200

    feat: added fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from 'expo-router';
+import {SplashScreen, Stack} from 'expo-router';
+import {useFonts, Manrope_400Regular, Manrope_500Medium, Manrope_700Bold} from "@expo-google-fonts/manrope";
+import {useEffect} from "react";
 
 export default function RootLayout() {
+    const [loaded, error] = useFonts({
+        'Manrope-Regular': Manrope_400Regular,
+        'Manrope-Medium': Manrope_500Medium,
+        'Manrope-Bold': Manrope_700Bold,
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync()
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null
+    }
+
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
